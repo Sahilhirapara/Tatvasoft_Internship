28894731956ad9b5458b071fcfa5217d0c2199bb
Sahilhirapara <126833776+Sahilhirapara@users.noreply.github.com>
2023-06-07 14:17:33 +0530

    Delete day 5 directory

--- day 5/src/components/validationErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const ValidationErrorMessage = (props) => {
-    return(
-        <>{props.touched && <p> {props.message}</p>}</>
-    );
-};
-export default ValidationErrorMessage;
